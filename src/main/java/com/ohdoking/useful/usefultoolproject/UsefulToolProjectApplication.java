7ece640b93f3cf9681c00964b1e7c42ff133d96e
Kenny Oh <ohdoking@gmail.com>
2018-11-30 17:28:09 +0100

    git init

---
+++ src/main/java/com/ohdoking/useful/usefultoolproject/UsefulToolProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ohdoking.useful.usefultoolproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefulToolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsefulToolProjectApplication.class, args);
+	}
+}
