3d441d863dd2ac0ce025f10aaa41b242dde8f0d2
Kenny Oh <ohdoking@gmail.com>
2018-12-03 15:21:35 +0100

    add how to define cron comments

--- src/main/java/com/ohdoking/useful/usefultoolproject/schedule/ScheduledTasks.java
+++ src/main/java/com/ohdoking/useful/usefultoolproject/schedule/ScheduledTasks.java
@@ -20,6 +20,23 @@ import org.springframework.stereotype.Component;
 import com.ohdoking.useful.usefultoolproject.dto.SearchRank;
 import com.ohdoking.useful.usefultoolproject.repository.SearchRankRepository;
 
+/**
+ * 
+ * for Scheduling class 
+ * 
+ * how to define cron.
+ * 
+ *  "0 0 * * * *" = the top of every hour of every day.
+ *  *"/10 * * * * *" = every ten seconds.
+ *	"0 0 8-10 * * *" = 8, 9 and 10 o'clock of every day.
+ *	"0 0 6,19 * * *" = 6:00 AM and 7:00 PM every day.
+ *	"0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30, 10:00 and 10:30 every day.
+ *	"0 0 9-17 * * MON-FRI" = on the hour nine-to-five weekdays
+ *	"0 0 0 25 12 ?" = every Christmas Day at midnight
+ *
+ * @author ohdoking
+ *
+ */
 @Component
 public class ScheduledTasks {
 
@@ -37,7 +54,8 @@ public class ScheduledTasks {
      * Naver(http://www.naver.com) Top 20 research word crawling every 1 hour
      */
     //the top of every hour of every day.
-    @Scheduled(cron = "0 0 * * * *")
+    //@Scheduled(cron = "0 0 * * * *")
+    @Scheduled(fixedRate = 50000)
     public void reportCurrentTime() {
     	String crwaObjectUrl = env.getProperty("crawl.url");
     	
