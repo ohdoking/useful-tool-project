5f5ebd9ad2ca66e2f57f3d144c0ed73921949db4
Kenny Oh <ohdoking@gmail.com>
2018-12-05 00:14:58 +0100

    set swagger for providing API information.

--- src/main/java/com/ohdoking/useful/usefultoolproject/resources/SearchRankResource.java
+++ src/main/java/com/ohdoking/useful/usefultoolproject/resources/SearchRankResource.java
@@ -25,34 +25,40 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "/searchRanks" )
+@Api(value = "SearchRankResource", description = "API related in SearchRank",basePath = "/searchRanks")
 public class SearchRankResource {
 	
 	@Autowired
 	private SearchRankRepository searchRankRepository;
 	
 	@GetMapping
+	@ApiOperation(value = "retrieve All Search Ranks List", notes = "API that retrieve All Search Ranks List.")
 	public Page<SearchRank> retrieveAllSearchRanks(Pageable pageable) {
 		return searchRankRepository.findAll(pageable);
 	}
 	
 	@GetMapping("/detail")
+	@ApiOperation(value = "retrieve Search Ranks List by searchword", notes = "API that retrieve All Search Ranks List by searchword.")
 	public List<SearchRank> retrieveSearchRanksBySearchWord(@RequestParam String searchWord, Sort sort) {
 		return searchRankRepository.findBysearchWordLike(searchWord, sort);
 	}
 	
 	
 	@GetMapping("/{id}")
+	@ApiOperation(value = "retrieve SearchRank Item by id", notes = "API that retrieve SearchRank Item by id.")
 	public SearchRank retrieveSearchRank(@PathVariable Long id) {
 		return searchRankRepository.findById(id)
 				.orElseThrow(() -> new SearchRankNotFoundException(id));
 	}
 	
 	@PostMapping
+	@ApiOperation(value = "save new SearchRank Item", notes = "API that save new SearchRank Item.")
 	public SearchRank newSearchRank(@RequestBody SearchRank searchRank) {
 		return searchRankRepository.save(searchRank);
 	}
 	
 	@PutMapping("/{id}")
+	@ApiOperation(value = "replace new SearchRank Item", notes = "API that replace new SearchRank Item.")
 	public SearchRank replaceEmployee(@RequestBody SearchRank newSearchRank, @PathVariable Long id) {
 
 		return searchRankRepository.findById(id)
@@ -71,6 +77,7 @@ public class SearchRankResource {
 	}
 
 	@DeleteMapping("/{id}")
+	@ApiOperation(value = "delete new SearchRank Item", notes = "API that delete new SearchRank Item.")
 	public void deleteEmployee(@PathVariable Long id) {
 		searchRankRepository.deleteById(id);
 	}
