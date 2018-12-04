33c2048c01be2e715fc697759fcc7af6d0945918
Kenny Oh <ohdoking@gmail.com>
2018-12-04 22:53:09 +0100

    add comment in SearchRankNotFoundAdvice

--- src/main/java/com/ohdoking/useful/usefultoolproject/advice/SearchRankNotFoundAdvice.java
+++ src/main/java/com/ohdoking/useful/usefultoolproject/advice/SearchRankNotFoundAdvice.java
@@ -8,6 +8,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.ohdoking.useful.usefultoolproject.exception.SearchRankNotFoundException;
 
+/**
+ * 
+ * 
+ * @author ohdoking
+ *
+ *
+	@ResponseBody signals that this advice is rendered straight into the response body.
+	@ExceptionHandler configures the advice to only respond if an EmployeeNotFoundException is thrown.
+	@ResponseStatus says to issues an HttpStatus.NOT_FOUND, i.e. an HTTP 404.
+	The body of the advice generates the content. In this case, it gives the message of the exception.
+ */
 @ControllerAdvice
 class SearchRankNotFoundAdvice {
 
